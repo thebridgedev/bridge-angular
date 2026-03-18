c6dcec98161a39cba1be258af80b1f8552160ed5
Iman Pouya <iman@nebulr.group>
2026-03-18 18:36:44 +0100

    fix: update test local environment config

--- demo/src/environments/environment.test.local.ts
+++ demo/src/environments/environment.test.local.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  bridgeAppId: '',
+  bridgeAppId: '699d84b10dc500dac9c6f93b',
   bridgeCallbackUrl: 'http://localhost:3001/auth/oauth-callback',
   bridgeDebug: true,
   authBaseUrl: 'http://localhost:3200/auth' as string | undefined,
